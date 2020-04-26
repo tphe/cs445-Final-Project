8776f4b4eb734cf13a1bfe5aeb7d09ab13093a63
tphe <tphe@users.noreply.github.com>
2020-04-26 16:34:34 -0500

    First round of edits
    Updated most functions. The final output is just a black image, so working on getting that debugged, but everything runs.

--- sketcher.py
+++ sketcher.py
@@ -11,18 +11,24 @@ import cv2
 import numpy as np
 #%matplotlib inline
 import matplotlib.pyplot as plt
-from utils import *
+import math
 import os
 import scipy
 import scipy.sparse.linalg
+import PIL
+from PIL import Image, ImageDraw
 
 al_img = cv2.cvtColor(cv2.imread('images/alfred.png'), cv2.COLOR_BGR2RGB)
-al_img = cv2.cvtColor(al_img, cv2.COLOR_BGR2GRAY).astype('double') / 255.0
+al_img = cv2.cvtColor(al_img, cv2.COLOR_BGR2GRAY)
 plt.imshow(al_img, cmap="gray")
 
 def get_gradient(img):
     #gets the gradient image
     grad_img = cv2.Canny(img,100,200)
+
+    grad_img[grad_img == 255] = 1
+    grad_img[grad_img == 0] = 255
+    grad_img[grad_img == 1] = 0
 
     plt.imshow(grad_img, cmap="gray")
     
@@ -34,20 +40,67 @@ def gradient_prioritize(grad_img):
     #look into the Canny edge detector algorithm for blob/length detection
     h, w = grad_img.shape
     
-    s = 1
+    #s = 0
     
-    priority = np.zeros(grad_img.shape)
+    priority = []
     
-    for i in range(len(h)):
-        for j in range(len(w)):
-            if grad_img[h,w] > 0:
-                priority[h,w] = s
-                s += 1
+    for i in range(h):
+        for j in range(w):
+            if grad_img[i,j] == 0:
+                priority.append((i,j))
+                #s += 1
+    
+    #priority = priority[:s]
     
     return priority
 
 def sketch_to_original(grad_img, img):
-    #creates gif of merge between gradient image an original
+    #creates a series of images merging between gradient image and original
+    grad_img = grad_img.astype(float)
+    nf = 20
+    img_diff = (img - grad_img)
+    img_diff /= nf
+    h, w = grad_img.shape
+    
+    #output_series = np.zeros((h, w, nf))
+    output_series = []
+    
+    for i in range(nf):
+        grad_img += img_diff
+        #output_series[:,:,i] = np.rint(grad_img).astype(int)
+        output_series.append(Image.fromarray(cv2.cvtColor(
+                np.rint(grad_img).astype(np.uint8), cv2.COLOR_GRAY2RGB)))
+
+    return output_series        
+    
 
 def gif_creator(grad_img, priority, speed):
     #creates gif of sketching, with pixes added at rate of speed parameter
+    sketch_img = np.zeros(grad_img.shape, dtype = np.uint8)
+    h, w = grad_img.shape
+    pix = 0
+
+    frames = math.ceil(len(priority)/speed)
+    
+    output_series = []
+    
+    for i in range(frames):
+        for j in range(speed):
+            y, x = priority[pix]
+            sketch_img[y, x] = grad_img[y, x]
+            output_series.append(Image.fromarray(cv2.cvtColor(
+                sketch_img, cv2.COLOR_GRAY2RGB)))
+            pix += 1
+        if i == frames - 2:
+            speed = len(priority) - pix - 1
+    
+    output_series[0].save('C:/users/tom/documents/github/cs445-final-project/output/al_sketch.gif', save_all = True,
+                          append_images = output_series[1:], duration = 40)
+    
+            
+    
+    
+    
+    
+    
+    
